c326ff2c8cb57b8b741afbfa8b21c4d2dd6d0fe1
kishore280 <maheshwarankishore@gmail.com>
2026-06-13 16:09:11 +0530

    ahem user

--- backend/src/User.ts
+++ backend/src/User.ts
@@ -1,5 +1,23 @@
+import { WebSocket} from "ws";
+import {randomUUID} from "crypto";
+import type { Socket } from "dgram";
+
 export class User{
-    id : unique,
-    name: string,
-    socket: WebSocket
+    public id : string;
+    public name: string;
+    public socket: WebSocket;
+
+
+    constructor(socket: WebSocket, name?: string){
+        this.id = randomUUID();
+        this.name = name?? 'guest';
+        this. socket = socket;
+    }
+
+    send(message: Object){
+        const connectionOpen = this.socket.readyState === WebSocket.OPEN;
+        if (connectionOpen){
+            this.socket.send(JSON.stringify(message));
+        }
+    }
 }
