0fe09a3787ee722518ac25791be169fa8b3f7d69
kishore280 <maheshwarankishore@gmail.com>
2026-06-13 11:53:15 +0530

    improved welcome

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import { WebSocketServer } from 'ws';
 import { GameManager } from './GameManager.js';
+import { WELCOME } from './messages.js';
 
 const wss = new WebSocketServer({ port: 8080 });
 
@@ -9,7 +10,12 @@ wss.on('connection', function connection(ws) {
   gameManager.addUser(ws)
   ws.on('error', console.error);
   ws.on("close", () => gameManager.removeUser(ws))
-  ws.send('welcome');
+  ws.send(JSON.stringify({
+    type: WELCOME,
+    payload: {
+      message: 'Welcome to the chess server! You are connected to the server.'
+    }
+  }));
 });
 
 console.log('Server is running on port 8080');
