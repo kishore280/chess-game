baa27bcb6a1893cb29a7e76f8d2cb56f73c0fdf2
kishore280 <maheshwarankishore@gmail.com>
2026-06-14 22:54:49 +0530

    register and login endpoints

--- apps/http/src/index.ts
+++ apps/http/src/index.ts
@@ -1,10 +1,13 @@
+import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
+import authRouter from './routes/auth.js'
 
 const PORT = 3001
 const app = express();
 app.use(cors())
 app.use(express.json())
+app.use('/auth', authRouter)
 
 app.get('/',(req,res)=>{
     res.send("hi")
--- apps/http/src/routes/auth.ts
+++ apps/http/src/routes/auth.ts
@@ -1,22 +1,34 @@
-import { Router } from 'express'
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
-import { validate } from '../middleware/validate'
-import { loginSchema, registerSchema } from '../schemas/authSchema'
-import { db, users } from '@chess/db'
+import { Router } from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { validate } from "../middleware/validate";
+import { loginSchema, registerSchema } from "../schemas/authSchema";
+import { db, users, eq } from "@chess/db";
 
-const router = Router()
+const router = Router();
 
-router.post('/register', validate(registerSchema), async (req, res) => {
-    const { username, email, password } = req.body
-    const passwordHash = await bcrypt.hash(password, 10)
-    const [user] = await db.insert(users).values({ username, email, passwordHash }).returning()
-    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET!)
-    res.json({ token })
-})
+router.post("/register", validate(registerSchema), async (req, res) => {
+  const { username, email, password } = req.body;
+  const passwordHash = await bcrypt.hash(password, 10);
+  const [user] = await db
+    .insert(users)
+    .values({ username, email, passwordHash })
+    .returning();
+  const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET!);
+  res.json({ token });
+});
 
-router.post('/login', validate(loginSchema), async (req, res) => {
-    res.json({ message: "Login successful" })
-})
+router.post("/login", validate(loginSchema), async (req, res) => {
+  const { email, password } = req.body;
+  const [user] = await db.select().from(users).where(eq(users.email, email)).limit(1)
+  if (!user) { res.status(404).json({ error: "User not found" }); return }
+  const passCorrect = await bcrypt.compare(password, user.passwordHash);
+  if (!passCorrect) {
+    res.status(401).json({ error: "Invalid password" });
+    return;
+  }
+  const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET!);
+  res.json({ message: "Login successful", token });
+});
 
-export default router
+export default router;
