e1500d233b68a66f5aeac65496e0f9d0dd226f38
kishore280 <maheshwarankishore@gmail.com>
2026-06-14 16:58:45 +0530

    hah dumy

--- apps/http/src/routes/auth.ts
+++ apps/http/src/routes/auth.ts
@@ -6,10 +6,22 @@ router.post('/register',(req,res)=>{
     const {username, email, password} = req.body
 
 
+
+    res.json({
+        message: "Resgistered"
+    })
+
+
 })
 
 
 router.post('/login', (req,res)=>{
+
+
+
+    res.json({
+        message: "Login succesfull"
+    })
 
 })
 
